c25667e808056c6acb4b8fd1b4c14dfab8ba4f7e
samuelwsz <samuelxwsz@gmail.com>
2024-02-08 20:01:11 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,56 @@ import "./globals.css"
 import logo from "./assets/logo-nlw-expert.svg"
 import NoteCard from "./components/note-card"
 import NewNoteCard from "./components/new-note-card"
+import { ChangeEvent, useState } from "react"
+
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
 
 function App() {
+  const [search, setSearch] = useState("")
+  const [notes, setNotes] = useState<Note[]>(() => {
+    // salvar notas no localstorage do navegador
+    const notesOnStorage = localStorage.getItem("notes")
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(), // é gerado um id único
+      date: new Date(),
+      content,
+    }
+
+    // [newNote, ...notes] o newNote esta antes do ...notes pois quero que as informações novas aparecam antes das ja fornecidas
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    // salvar notas no localstorage do navegador
+    localStorage.setItem("notes", JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -13,14 +61,18 @@ function App() {
           type="text"
           placeholder="Busque suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard note={{ content: "aa", date: new Date() }} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes.map((note) => {
+          return <NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from "lucide-react"
 import { ChangeEvent, FormEvent, useState } from "react"
 import { toast } from "sonner"
 
-export default function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+export default function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
   const [content, setContent] = useState("")
 
@@ -22,9 +26,12 @@ export default function NewNoteCard() {
   function handleSaveNote(event: FormEvent) {
     event.preventDefault()
 
-    console.log(content)
+    onNoteCreated(content)
 
-    toast.success('Nota criada com sucesso')
+    setContent("")
+    setShouldShowOnboarding(true)
+
+    toast.success("Nota criada com sucesso")
   }
 
   return (
@@ -75,6 +82,7 @@ export default function NewNoteCard() {
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChange}
+                  value={content}
                 />
               )}
             </div>
